e633e0d3f8ae50485def19b1d588eaf008716738
Nick Frost <nickfrostatx@gmail.com>
2016-06-06 22:25:46 -0700

    No strict host key checking

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -5,5 +5,7 @@ chmod 700 travis
 openssl aes-256-cbc -K $encrypted_fa51861454fb_key -iv $encrypted_fa51861454fb_iv -in travis/deploy.enc -out travis/deploy -d
 chmod 600 travis/deploy
 ssh-add travis/deploy
+mkdir -m 700 -p ~/.ssh
+echo -e "Host *\n\tStrictHostKeyChecking no\n" > ~/.ssh/config
 git remote add deploy "git@thewhitehat.club:go/src/github.com/WhiteHatCP/seclab-listener"
 git push deploy
