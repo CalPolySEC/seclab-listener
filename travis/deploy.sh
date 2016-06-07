1d07a5b7e74e9a0d29b69aa17ecb7073ec02b75c
Nick Frost <nickfrostatx@gmail.com>
2016-06-06 22:22:08 -0700

    Fix SSH permissions for Travis

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
 eval "$(ssh-agent -s)"
+chmod 700 travis
 openssl aes-256-cbc -K $encrypted_fa51861454fb_key -iv $encrypted_fa51861454fb_iv -in travis/deploy.enc -out travis/deploy -d
+chmod 600 travis/deploy
 ssh-add travis/deploy
 git remote add deploy "git@thewhitehat.club:go/src/github.com/WhiteHatCP/seclab-listener"
 git push deploy
